23140b26d5afcfe4f6781a905c3028458d18283c
jayesh <jayes3571@gmail.com>
2024-06-07 23:40:11 +0530

    Add GenericRepositroy and GenericInterface

---
+++ EFCoreWenAPI/EFCoreWenAPI/Interface/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreWenAPI.Interface
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetAsync(string id);        
+        Task<T> AddEntity(T entity);
+        Task<T> UpdateEntity(T entity);
+        Task<T> DeleteEntity(int id);
+    }
+}
--- EFCoreWenAPI/EFCoreWenAPI/obj/Debug/net8.0/EFCoreWenAPI.AssemblyInfo.cs
+++ EFCoreWenAPI/EFCoreWenAPI/obj/Debug/net8.0/EFCoreWenAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreWenAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81795184fe6c1d970ec5ec47fb0699e515a4a187")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreWenAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreWenAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
